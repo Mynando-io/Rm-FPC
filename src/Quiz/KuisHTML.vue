637a15919ce38943823cfaaf0ca22122d503d055
3.sahabat.002275.rq1 <3.sahabat.002275.rq1@gmail.com>
2025-04-26 06:40:57 +0000

    new

--- src/Quiz/KuisHTML.vue
+++ src/Quiz/KuisHTML.vue
@@ -1,23 +1,47 @@
 <script setup>
+import { ref, computed } from 'vue'
 
-const count = 'Saya Kuis HTML'
+const kuisTr = ref(false) // adalah state note True
+const kuisFl = ref(false) // adalah state note false
+
+// Menimpa nilai awal menjadi nilai baru
+function SkuisTr(){ // Ini untuk buka catatan kuis True
+  kuisTr.value = !kuisTr.value
+}
+
+// Menimpa nilai awal menjadi nilai baru
+function SkuisFl(){ // Ini untuk buka catatan kuis False
+  kuisFl.value = !kuisFl.value
+}
 
 </script>
 
 
 
 <template>
+<!-- ======================= Awal  dari Template ======================= -->
 
-<h1>{{ count }}</h1>
+<h1>Exercise</h1>
 
-<p>Selamat datang di HTML Page</p>
+<p>Soal: </p>
+<p>True or False. Empty elements must have a close tag.</p>
 
+
+
+<lu>
+  <li><button @click="SkuisTr">True</button></li>
+  <li><button @click="SkuisFl">False</button></li>
+</lu>
+
+{{ kuisTr ? 'Ini adalah catatan True' : '' }}
+<br>
+{{ kuisFl ? 'Ini adalah catatan False' : '' }}
 
 <nav>
     <RouterLink to="/html">Go too Home</RouterLink>
   </nav>
 
-
+<!-- ======================= Akhir dari Template ======================= -->
 </template>
 
 
