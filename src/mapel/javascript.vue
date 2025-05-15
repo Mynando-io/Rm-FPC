870adbe1720d0a6cdcca33fadbec2dd51264e01e
pitribn <pitribn@gmail.com>
2025-05-15 03:16:42 +0000

    dasarjavascript

--- src/mapel/javascript.vue
+++ src/mapel/javascript.vue
@@ -11,6 +11,7 @@
 
 <template>
 <!-- 0000000000000000000000000000000000000000000000000000000 Template Area start 0000000000000000000000000000000000000000-->
+<h1 id="sample-markdown">Sample Markdown</h1>
 <h1 id="javascript">Javascript</h1>
 <h2 id="table-of-content">Table of content</h2>
 <ul>
@@ -1262,8 +1263,34 @@ points.sort(function(a, b){return b - a});
 <li>freecodecamp </li>
 <li>mozzila</li>
 <li>chatgpt</li>
-<li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get</a> (get dan set)</li>
+<li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get</a> (get dan set)
+This is some basic, sample markdown.</li>
 </ul>
+<h2 id="second-heading">Second Heading</h2>
+<ul>
+<li>Unordered lists, and:<ol>
+<li>One</li>
+<li>Two</li>
+<li>Three</li>
+</ol>
+</li>
+<li>More</li>
+</ul>
+<blockquote>
+<p>Blockquote</p>
+</blockquote>
+<p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href="https://markdowntohtml.com">A link</a> to somewhere.</p>
+<p>And code highlighting:</p>
+<pre><code class="lang-js"><span class="hljs-keyword">var</span> foo = <span class="hljs-string">'bar'</span>;
+
+<span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">baz</span><span class="hljs-params">(s)</span> </span>{
+   <span class="hljs-keyword">return</span> foo + <span class="hljs-string">':'</span> + s;
+}
+</code></pre>
+<p>Or inline code like <code>var foo = &#39;bar&#39;;</code>.</p>
+<p>Or an image of bears</p>
+<p><img src="http://placebear.com/200/200" alt="bears"></p>
+<p>The end ...</p>
 
 
 
